91d1d6c7fb5f77d2148406a9348d297149631617
jemygraw <jemygraw@gmail.com>
2015-05-04 12:55:19 +0800

    Fix the error in resumable upload which can cause the corruption of the binary files when halted and resumed.

--- Qiniu/Qiniu/Storage/ResumeUploader.cs
+++ Qiniu/Qiniu/Storage/ResumeUploader.cs
@@ -208,6 +208,7 @@ namespace Qiniu.Storage
             }
 
             long offset = recoveryFromResumeRecord();
+            this.fileStream.Seek(offset, SeekOrigin.Begin);
             this.nextTask(offset, 0, Config.UPLOAD_HOST);
         }
         #endregion
@@ -220,7 +221,6 @@ namespace Qiniu.Storage
         {
             try
             {
-                this.fileStream.Seek(0, SeekOrigin.Begin);
                 this.lastModifyTime = DateTime.Now.ToFileTime();
                 this.size = this.fileStream.Length;
                 long blockCount = (this.size % Config.BLOCK_SIZE == 0) ? (this.size / Config.BLOCK_SIZE) : (this.size / Config.BLOCK_SIZE + 1);
@@ -231,8 +231,9 @@ namespace Qiniu.Storage
                 this.upCompletionHandler(this.key, ResponseInfo.fileError(ex), "");
                 return;
             }
-
+    
             long offset = recoveryFromResumeRecord();
+            this.fileStream.Seek(offset, SeekOrigin.Begin);
             this.nextTask(offset, 0, Config.UPLOAD_HOST);
         }
         #endregion
